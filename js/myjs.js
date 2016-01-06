84eacdfb57bb32dbb5267a8ec3401725f705a1be
Neftalí Acosta <hola@neftaliacosta.com>
2016-01-06 15:28:54 -0600

    Version 1.1
    Se agregó la sección de clientes  pendientes y adicional a eso se
    incluye un carousel de citas de twitter.

--- js/myjs.js
+++ js/myjs.js
@@ -1,4 +1,18 @@
-﻿/* Js Facebook*/
+﻿
+/*crouseles*/
+$(document).ready(function () {
+    alert("ey");
+    $('#carousel1').carousel({
+        interval: 3999
+    });
+
+
+    $('#carousel2').carousel({
+        interval: 4000
+    });
+});
+
+/* Js Facebook*/
 
 (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
@@ -16,6 +30,7 @@ _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
     $.src="//v2.zopim.com/?3anqSB90R9klm0u9AVGgMUkI8CmnDJ5X";z.t=+new Date;$.
     type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
 
-/*zopin end*/
+
+
 
 
