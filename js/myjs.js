cd067344f382c0ca0da8c87016e83a6bbe3ea98d
Neftalí Acosta <hola@neftaliacosta.com>
2016-01-06 10:20:44 -0600

    Version1
    Sitio preliminar, falta agregar la sección de clientes. Hacer funcionar
    el formulario de contacto y el newsletter.

---
+++ js/myjs.js
@@ -0,0 +1,21 @@
+﻿/* Js Facebook*/
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=556541261138043";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+/*zopin*/
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+    $.src="//v2.zopim.com/?3anqSB90R9klm0u9AVGgMUkI8CmnDJ5X";z.t=+new Date;$.
+    type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+
+/*zopin end*/
+
+
